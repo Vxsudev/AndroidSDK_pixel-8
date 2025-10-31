9b4124c0cd468d40853c6a67fb3e5fc69910af02
Vxsudev <vp@raystrat.com>
2025-10-31 16:00:07 +0530

    Upgrade root build.gradle.kts to AGP 8.9.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
-// Top-level Gradle build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+kplugins {
+    id("com.android.application") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
+
