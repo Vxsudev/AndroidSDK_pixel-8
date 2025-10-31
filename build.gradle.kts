ebce6a83f98ea34832aa54c903f03bebca489bfa
Vxsudev <vp@raystrat.com>
2025-10-31 16:20:50 +0530

    Lock root plugins to AGP 8.9.1 + Kotlin 1.9.24 + Google Services 4.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
-kplugins {
+plugins {
     id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
-task<Delete>("clean") {
+tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
 
